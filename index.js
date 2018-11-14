4d7d6a3bb2a1d3778512a4c4159f9d6a9e444ee6
Samuel Amoah <snamoah@gmail.com>
2018-11-14 16:59:10 +0000

    move ping to different file

--- index.js
+++ index.js
@@ -2,18 +2,13 @@
 
 'use strict'
 
-const ping = require('ping')
-const beep = require('beepbeep')
+const program = require('commander')
+const { version } = require('./package.json')
 
-const DEFAULT_HOST = 'google.com'
-
-const last = 200
-const baseline = [ 200, 200, 200 ]
-const interlude = [ 50, 50, 50, 50 ]
-
-const offlineBeep = _ =>
-  beep([...baseline, ...interlude, last])
-
-ping.sys.probe(DEFAULT_HOST, isAlive => { 
-  isAlive || offlineBeep() 
-})
+program
+  .version(version)
+  .arguments('<command>', 'Turn Offline alert on/off')
+  .action(command => {
+    
+  })
+  .parse(process.argv)
